654fd3a134e3d7006d519bd7909f27c028dbd2e9
Gerard Rius <gerard.rius.husillos@estudiantat.upc.edu>
2025-01-05 15:43:23 +0100

    updated glfw and zig versions

--- README.md
+++ README.md
@@ -6,14 +6,14 @@ Run `zig fetch --save git+https://github.com/griush/zglfw`.
 
 Add in `build.zig`:
 ```zig
+// module links libc
 const zglfw = b.dependency("zglfw", .{});
-exe.root_module.addAnonymousImport("glfw", .{
+module.addAnonymousImport("glfw", .{
     .root_source_file = zglfw.path("glfw3.zig"),
     .target = target,
     .optimize = optimize,
 });
 
-exe.linkLibC();
 if (builtin.os.tag == .windows) {
     exe.addLibraryPath(zglfw.path("bin/windows/"));
     exe.linkSystemLibrary("Gdi32");
--- glfw3.zig
+++ glfw3.zig
@@ -78,37 +78,37 @@ pub const int_fast8_t = i8;
 pub const uint_fast8_t = u8;
 pub const intmax_t = c_longlong;
 pub const uintmax_t = c_ulonglong;
-pub const GLFWglproc = ?*const fn () callconv(.C) void;
-pub const GLFWvkproc = ?*const fn () callconv(.C) void;
+pub const GLFWglproc = ?*const fn () callconv(.c) void;
+pub const GLFWvkproc = ?*const fn () callconv(.c) void;
 pub const struct_GLFWmonitor = opaque {};
 pub const GLFWmonitor = struct_GLFWmonitor;
 pub const struct_GLFWwindow = opaque {};
 pub const GLFWwindow = struct_GLFWwindow;
 pub const struct_GLFWcursor = opaque {};
 pub const GLFWcursor = struct_GLFWcursor;
-pub const GLFWallocatefun = ?*const fn (usize, ?*anyopaque) callconv(.C) ?*anyopaque;
-pub const GLFWreallocatefun = ?*const fn (?*anyopaque, usize, ?*anyopaque) callconv(.C) ?*anyopaque;
-pub const GLFWdeallocatefun = ?*const fn (?*anyopaque, ?*anyopaque) callconv(.C) void;
-pub const GLFWerrorfun = ?*const fn (c_int, [*c]const u8) callconv(.C) void;
-pub const GLFWwindowposfun = ?*const fn (?*GLFWwindow, c_int, c_int) callconv(.C) void;
-pub const GLFWwindowsizefun = ?*const fn (?*GLFWwindow, c_int, c_int) callconv(.C) void;
-pub const GLFWwindowclosefun = ?*const fn (?*GLFWwindow) callconv(.C) void;
-pub const GLFWwindowrefreshfun = ?*const fn (?*GLFWwindow) callconv(.C) void;
-pub const GLFWwindowfocusfun = ?*const fn (?*GLFWwindow, c_int) callconv(.C) void;
-pub const GLFWwindowiconifyfun = ?*const fn (?*GLFWwindow, c_int) callconv(.C) void;
-pub const GLFWwindowmaximizefun = ?*const fn (?*GLFWwindow, c_int) callconv(.C) void;
-pub const GLFWframebuffersizefun = ?*const fn (?*GLFWwindow, c_int, c_int) callconv(.C) void;
-pub const GLFWwindowcontentscalefun = ?*const fn (?*GLFWwindow, f32, f32) callconv(.C) void;
-pub const GLFWmousebuttonfun = ?*const fn (?*GLFWwindow, c_int, c_int, c_int) callconv(.C) void;
-pub const GLFWcursorposfun = ?*const fn (?*GLFWwindow, f64, f64) callconv(.C) void;
-pub const GLFWcursorenterfun = ?*const fn (?*GLFWwindow, c_int) callconv(.C) void;
-pub const GLFWscrollfun = ?*const fn (?*GLFWwindow, f64, f64) callconv(.C) void;
-pub const GLFWkeyfun = ?*const fn (?*GLFWwindow, c_int, c_int, c_int, c_int) callconv(.C) void;
-pub const GLFWcharfun = ?*const fn (?*GLFWwindow, c_uint) callconv(.C) void;
-pub const GLFWcharmodsfun = ?*const fn (?*GLFWwindow, c_uint, c_int) callconv(.C) void;
-pub const GLFWdropfun = ?*const fn (?*GLFWwindow, c_int, [*c][*c]const u8) callconv(.C) void;
-pub const GLFWmonitorfun = ?*const fn (?*GLFWmonitor, c_int) callconv(.C) void;
-pub const GLFWjoystickfun = ?*const fn (c_int, c_int) callconv(.C) void;
+pub const GLFWallocatefun = ?*const fn (usize, ?*anyopaque) callconv(.c) ?*anyopaque;
+pub const GLFWreallocatefun = ?*const fn (?*anyopaque, usize, ?*anyopaque) callconv(.c) ?*anyopaque;
+pub const GLFWdeallocatefun = ?*const fn (?*anyopaque, ?*anyopaque) callconv(.c) void;
+pub const GLFWerrorfun = ?*const fn (c_int, [*c]const u8) callconv(.c) void;
+pub const GLFWwindowposfun = ?*const fn (?*GLFWwindow, c_int, c_int) callconv(.c) void;
+pub const GLFWwindowsizefun = ?*const fn (?*GLFWwindow, c_int, c_int) callconv(.c) void;
+pub const GLFWwindowclosefun = ?*const fn (?*GLFWwindow) callconv(.c) void;
+pub const GLFWwindowrefreshfun = ?*const fn (?*GLFWwindow) callconv(.c) void;
+pub const GLFWwindowfocusfun = ?*const fn (?*GLFWwindow, c_int) callconv(.c) void;
+pub const GLFWwindowiconifyfun = ?*const fn (?*GLFWwindow, c_int) callconv(.c) void;
+pub const GLFWwindowmaximizefun = ?*const fn (?*GLFWwindow, c_int) callconv(.c) void;
+pub const GLFWframebuffersizefun = ?*const fn (?*GLFWwindow, c_int, c_int) callconv(.c) void;
+pub const GLFWwindowcontentscalefun = ?*const fn (?*GLFWwindow, f32, f32) callconv(.c) void;
+pub const GLFWmousebuttonfun = ?*const fn (?*GLFWwindow, c_int, c_int, c_int) callconv(.c) void;
+pub const GLFWcursorposfun = ?*const fn (?*GLFWwindow, f64, f64) callconv(.c) void;
+pub const GLFWcursorenterfun = ?*const fn (?*GLFWwindow, c_int) callconv(.c) void;
+pub const GLFWscrollfun = ?*const fn (?*GLFWwindow, f64, f64) callconv(.c) void;
+pub const GLFWkeyfun = ?*const fn (?*GLFWwindow, c_int, c_int, c_int, c_int) callconv(.c) void;
+pub const GLFWcharfun = ?*const fn (?*GLFWwindow, c_uint) callconv(.c) void;
+pub const GLFWcharmodsfun = ?*const fn (?*GLFWwindow, c_uint, c_int) callconv(.c) void;
+pub const GLFWdropfun = ?*const fn (?*GLFWwindow, c_int, [*c][*c]const u8) callconv(.c) void;
+pub const GLFWmonitorfun = ?*const fn (?*GLFWmonitor, c_int) callconv(.c) void;
+pub const GLFWjoystickfun = ?*const fn (c_int, c_int) callconv(.c) void;
 pub const struct_GLFWvidmode = extern struct {
     width: c_int = @import("std").mem.zeroes(c_int),
     height: c_int = @import("std").mem.zeroes(c_int),
@@ -265,10 +265,10 @@ pub extern fn glfwVulkanSupported() c_int;
 pub extern fn glfwGetRequiredInstanceExtensions(count: [*c]u32) [*c][*c]const u8;
 pub const __llvm__ = @as(c_int, 1);
 pub const __clang__ = @as(c_int, 1);
-pub const __clang_major__ = @as(c_int, 18);
+pub const __clang_major__ = @as(c_int, 19);
 pub const __clang_minor__ = @as(c_int, 1);
-pub const __clang_patchlevel__ = @as(c_int, 6);
-pub const __clang_version__ = "18.1.6 (https://github.com/ziglang/zig-bootstrap 98bc6bf4fc4009888d33941daf6b600d20a42a56)";
+pub const __clang_patchlevel__ = @as(c_int, 0);
+pub const __clang_version__ = "19.1.0 (https://github.com/ziglang/zig-bootstrap 46b9e66db90230fe62404b27b85a378ccf2c82c2)";
 pub const __GNUC__ = @as(c_int, 4);
 pub const __GNUC_MINOR__ = @as(c_int, 2);
 pub const __GNUC_PATCHLEVEL__ = @as(c_int, 1);
@@ -300,7 +300,8 @@ pub const __FPCLASS_POSSUBNORMAL = @as(c_int, 0x0080);
 pub const __FPCLASS_POSNORMAL = @as(c_int, 0x0100);
 pub const __FPCLASS_POSINF = @as(c_int, 0x0200);
 pub const __PRAGMA_REDEFINE_EXTNAME = @as(c_int, 1);
-pub const __VERSION__ = "Clang 18.1.6 (https://github.com/ziglang/zig-bootstrap 98bc6bf4fc4009888d33941daf6b600d20a42a56)";
+pub const __VERSION__ = "Clang 19.1.0 (https://github.com/ziglang/zig-bootstrap 46b9e66db90230fe62404b27b85a378ccf2c82c2)";
+pub const __GXX_TYPEINFO_EQUALITY_INLINE = @as(c_int, 0);
 pub const __OBJC_BOOL_IS_BOOL = @as(c_int, 0);
 pub const __CONSTANT_CFSTRINGS__ = @as(c_int, 1);
 pub const __SEH__ = @as(c_int, 1);
@@ -356,14 +357,14 @@ pub const __INTMAX_TYPE__ = c_longlong;
 pub const __INTMAX_FMTd__ = "lld";
 pub const __INTMAX_FMTi__ = "lli";
 pub const __INTMAX_C_SUFFIX__ = @compileError("unable to translate macro: undefined identifier `LL`");
-// (no file):94:9
+// (no file):95:9
 pub const __UINTMAX_TYPE__ = c_ulonglong;
 pub const __UINTMAX_FMTo__ = "llo";
 pub const __UINTMAX_FMTu__ = "llu";
 pub const __UINTMAX_FMTx__ = "llx";
 pub const __UINTMAX_FMTX__ = "llX";
 pub const __UINTMAX_C_SUFFIX__ = @compileError("unable to translate macro: undefined identifier `ULL`");
-// (no file):100:9
+// (no file):101:9
 pub const __PTRDIFF_TYPE__ = c_longlong;
 pub const __PTRDIFF_FMTd__ = "lld";
 pub const __PTRDIFF_FMTi__ = "lli";
@@ -387,6 +388,7 @@ pub const __UINTPTR_FMTu__ = "llu";
 pub const __UINTPTR_FMTx__ = "llx";
 pub const __UINTPTR_FMTX__ = "llX";
 pub const __FLT16_DENORM_MIN__ = @as(f16, 5.9604644775390625e-8);
+pub const __FLT16_NORM_MAX__ = @as(f16, 6.5504e+4);
 pub const __FLT16_HAS_DENORM__ = @as(c_int, 1);
 pub const __FLT16_DIG__ = @as(c_int, 3);
 pub const __FLT16_DECIMAL_DIG__ = @as(c_int, 5);
@@ -401,6 +403,7 @@ pub const __FLT16_MIN_10_EXP__ = -@as(c_int, 4);
 pub const __FLT16_MIN_EXP__ = -@as(c_int, 13);
 pub const __FLT16_MIN__ = @as(f16, 6.103515625e-5);
 pub const __FLT_DENORM_MIN__ = @as(f32, 1.40129846e-45);
+pub const __FLT_NORM_MAX__ = @as(f32, 3.40282347e+38);
 pub const __FLT_HAS_DENORM__ = @as(c_int, 1);
 pub const __FLT_DIG__ = @as(c_int, 6);
 pub const __FLT_DECIMAL_DIG__ = @as(c_int, 9);
@@ -415,6 +418,7 @@ pub const __FLT_MIN_10_EXP__ = -@as(c_int, 37);
 pub const __FLT_MIN_EXP__ = -@as(c_int, 125);
 pub const __FLT_MIN__ = @as(f32, 1.17549435e-38);
 pub const __DBL_DENORM_MIN__ = @as(f64, 4.9406564584124654e-324);
+pub const __DBL_NORM_MAX__ = @as(f64, 1.7976931348623157e+308);
 pub const __DBL_HAS_DENORM__ = @as(c_int, 1);
 pub const __DBL_DIG__ = @as(c_int, 15);
 pub const __DBL_DECIMAL_DIG__ = @as(c_int, 17);
@@ -429,6 +433,7 @@ pub const __DBL_MIN_10_EXP__ = -@as(c_int, 307);
 pub const __DBL_MIN_EXP__ = -@as(c_int, 1021);
 pub const __DBL_MIN__ = @as(f64, 2.2250738585072014e-308);
 pub const __LDBL_DENORM_MIN__ = @as(c_longdouble, 3.64519953188247460253e-4951);
+pub const __LDBL_NORM_MAX__ = @as(c_longdouble, 1.18973149535723176502e+4932);
 pub const __LDBL_HAS_DENORM__ = @as(c_int, 1);
 pub const __LDBL_DIG__ = @as(c_int, 18);
 pub const __LDBL_DECIMAL_DIG__ = @as(c_int, 21);
@@ -462,7 +467,7 @@ pub const __INT64_TYPE__ = c_longlong;
 pub const __INT64_FMTd__ = "lld";
 pub const __INT64_FMTi__ = "lli";
 pub const __INT64_C_SUFFIX__ = @compileError("unable to translate macro: undefined identifier `LL`");
-// (no file):198:9
+// (no file):203:9
 pub const __UINT8_TYPE__ = u8;
 pub const __UINT8_FMTo__ = "hho";
 pub const __UINT8_FMTu__ = "hhu";
@@ -485,7 +490,7 @@ pub const __UINT32_FMTu__ = "u";
 pub const __UINT32_FMTx__ = "x";
 pub const __UINT32_FMTX__ = "X";
 pub const __UINT32_C_SUFFIX__ = @compileError("unable to translate macro: undefined identifier `U`");
-// (no file):220:9
+// (no file):225:9
 pub const __UINT32_MAX__ = @import("std").zig.c_translation.promoteIntLiteral(c_uint, 4294967295, .decimal);
 pub const __INT32_MAX__ = @import("std").zig.c_translation.promoteIntLiteral(c_int, 2147483647, .decimal);
 pub const __UINT64_TYPE__ = c_ulonglong;
@@ -494,7 +499,7 @@ pub const __UINT64_FMTu__ = "llu";
 pub const __UINT64_FMTx__ = "llx";
 pub const __UINT64_FMTX__ = "llX";
 pub const __UINT64_C_SUFFIX__ = @compileError("unable to translate macro: undefined identifier `ULL`");
-// (no file):228:9
+// (no file):233:9
 pub const __UINT64_MAX__ = @as(c_ulonglong, 18446744073709551615);
 pub const __INT64_MAX__ = @as(c_longlong, 9223372036854775807);
 pub const __INT_LEAST8_TYPE__ = i8;
@@ -589,6 +594,8 @@ pub const __USER_LABEL_PREFIX__ = "";
 pub const __FINITE_MATH_ONLY__ = @as(c_int, 0);
 pub const __GNUC_STDC_INLINE__ = @as(c_int, 1);
 pub const __GCC_ATOMIC_TEST_AND_SET_TRUEVAL = @as(c_int, 1);
+pub const __GCC_DESTRUCTIVE_SIZE = @as(c_int, 64);
+pub const __GCC_CONSTRUCTIVE_SIZE = @as(c_int, 64);
 pub const __CLANG_ATOMIC_BOOL_LOCK_FREE = @as(c_int, 2);
 pub const __CLANG_ATOMIC_CHAR_LOCK_FREE = @as(c_int, 2);
 pub const __CLANG_ATOMIC_CHAR16_T_LOCK_FREE = @as(c_int, 2);
@@ -623,9 +630,9 @@ pub const __x86_64__ = @as(c_int, 1);
 pub const __SEG_GS = @as(c_int, 1);
 pub const __SEG_FS = @as(c_int, 1);
 pub const __seg_gs = @compileError("unable to translate macro: undefined identifier `address_space`");
-// (no file):356:9
+// (no file):365:9
 pub const __seg_fs = @compileError("unable to translate macro: undefined identifier `address_space`");
-// (no file):357:9
+// (no file):366:9
 pub const __k8 = @as(c_int, 1);
 pub const __k8__ = @as(c_int, 1);
 pub const __tune_k8__ = @as(c_int, 1);
@@ -664,6 +671,7 @@ pub const __MOVDIRI__ = @as(c_int, 1);
 pub const __MOVDIR64B__ = @as(c_int, 1);
 pub const __PTWRITE__ = @as(c_int, 1);
 pub const __INVPCID__ = @as(c_int, 1);
+pub const __CRC32__ = @as(c_int, 1);
 pub const __AVX2__ = @as(c_int, 1);
 pub const __AVX__ = @as(c_int, 1);
 pub const __SSE4_2__ = @as(c_int, 1);
@@ -696,49 +704,52 @@ pub const __MINGW64__ = @as(c_int, 1);
 pub const __MSVCRT__ = @as(c_int, 1);
 pub const __MINGW32__ = @as(c_int, 1);
 pub const __declspec = @compileError("unable to translate C expr: unexpected token '__attribute__'");
-// (no file):427:9
-pub const _cdecl = @compileError("unable to translate macro: undefined identifier `__cdecl__`");
-// (no file):428:9
-pub const __cdecl = @compileError("unable to translate macro: undefined identifier `__cdecl__`");
-// (no file):429:9
-pub const _stdcall = @compileError("unable to translate macro: undefined identifier `__stdcall__`");
-// (no file):430:9
-pub const __stdcall = @compileError("unable to translate macro: undefined identifier `__stdcall__`");
-// (no file):431:9
-pub const _fastcall = @compileError("unable to translate macro: undefined identifier `__fastcall__`");
-// (no file):432:9
-pub const __fastcall = @compileError("unable to translate macro: undefined identifier `__fastcall__`");
-// (no file):433:9
-pub const _thiscall = @compileError("unable to translate macro: undefined identifier `__thiscall__`");
-// (no file):434:9
-pub const __thiscall = @compileError("unable to translate macro: undefined identifier `__thiscall__`");
-// (no file):435:9
-pub const _pascal = @compileError("unable to translate macro: undefined identifier `__pascal__`");
-// (no file):436:9
-pub const __pascal = @compileError("unable to translate macro: undefined identifier `__pascal__`");
 // (no file):437:9
+pub const _cdecl = @compileError("unable to translate macro: undefined identifier `__cdecl__`");
+// (no file):438:9
+pub const __cdecl = @compileError("unable to translate macro: undefined identifier `__cdecl__`");
+// (no file):439:9
+pub const _stdcall = @compileError("unable to translate macro: undefined identifier `__stdcall__`");
+// (no file):440:9
+pub const __stdcall = @compileError("unable to translate macro: undefined identifier `__stdcall__`");
+// (no file):441:9
+pub const _fastcall = @compileError("unable to translate macro: undefined identifier `__fastcall__`");
+// (no file):442:9
+pub const __fastcall = @compileError("unable to translate macro: undefined identifier `__fastcall__`");
+// (no file):443:9
+pub const _thiscall = @compileError("unable to translate macro: undefined identifier `__thiscall__`");
+// (no file):444:9
+pub const __thiscall = @compileError("unable to translate macro: undefined identifier `__thiscall__`");
+// (no file):445:9
+pub const _pascal = @compileError("unable to translate macro: undefined identifier `__pascal__`");
+// (no file):446:9
+pub const __pascal = @compileError("unable to translate macro: undefined identifier `__pascal__`");
+// (no file):447:9
 pub const __STDC__ = @as(c_int, 1);
 pub const __STDC_HOSTED__ = @as(c_int, 1);
 pub const __STDC_VERSION__ = @as(c_long, 201710);
 pub const __STDC_UTF_16__ = @as(c_int, 1);
 pub const __STDC_UTF_32__ = @as(c_int, 1);
+pub const __STDC_EMBED_NOT_FOUND__ = @as(c_int, 0);
+pub const __STDC_EMBED_FOUND__ = @as(c_int, 1);
+pub const __STDC_EMBED_EMPTY__ = @as(c_int, 2);
 pub const _DEBUG = @as(c_int, 1);
 pub const GLFW_INCLUDE_NONE = @as(c_int, 1);
 pub const _glfw3_h_ = "";
-pub const __STDDEF_H = "";
 pub const __need_ptrdiff_t = "";
 pub const __need_size_t = "";
 pub const __need_wchar_t = "";
 pub const __need_NULL = "";
 pub const __need_max_align_t = "";
 pub const __need_offsetof = "";
+pub const __STDDEF_H = "";
 pub const _PTRDIFF_T = "";
 pub const _SIZE_T = "";
 pub const _WCHAR_T = "";
 pub const NULL = @import("std").zig.c_translation.cast(?*anyopaque, @as(c_int, 0));
 pub const __CLANG_MAX_ALIGN_T_DEFINED = "";
 pub const offsetof = @compileError("unable to translate C expr: unexpected token 'an identifier'");
-// C:\zig\zig\0.13.0\files\lib\include/__stddef_offsetof.h:16:9
+// C:\zig\zig\0.14.0-dev.2599+ff4f2753e\files\lib\include/__stddef_offsetof.h:16:9
 pub const __CLANG_STDINT_H = "";
 pub const __int_least64_t = i64;
 pub const __uint_least64_t = u64;
@@ -751,19 +762,19 @@ pub const __uint_least8_t = u64;
 pub const __uint32_t_defined = "";
 pub const __int8_t_defined = "";
 pub const __stdint_join3 = @compileError("unable to translate C expr: unexpected token '##'");
-// C:\zig\zig\0.13.0\files\lib\include/stdint.h:287:9
+// C:\zig\zig\0.14.0-dev.2599+ff4f2753e\files\lib\include/stdint.h:291:9
 pub const __intptr_t_defined = "";
 pub const _INTPTR_T = "";
 pub const _UINTPTR_T = "";
 pub const __int_c_join = @compileError("unable to translate C expr: unexpected token '##'");
-// C:\zig\zig\0.13.0\files\lib\include/stdint.h:324:9
+// C:\zig\zig\0.14.0-dev.2599+ff4f2753e\files\lib\include/stdint.h:328:9
 pub inline fn __int_c(v: anytype, suffix: anytype) @TypeOf(__int_c_join(v, suffix)) {
     _ = &v;
     _ = &suffix;
     return __int_c_join(v, suffix);
 }
 pub const __uint_c = @compileError("unable to translate macro: undefined identifier `U`");
-// C:\zig\zig\0.13.0\files\lib\include/stdint.h:326:9
+// C:\zig\zig\0.14.0-dev.2599+ff4f2753e\files\lib\include/stdint.h:330:9
 pub const __int64_c_suffix = __INT64_C_SUFFIX__;
 pub const __int32_c_suffix = __INT64_C_SUFFIX__;
 pub const __int16_c_suffix = __INT64_C_SUFFIX__;
@@ -849,15 +860,15 @@ pub const INT_FAST8_MIN = __INT_LEAST8_MIN;
 pub const INT_FAST8_MAX = __INT_LEAST8_MAX;
 pub const UINT_FAST8_MAX = __UINT_LEAST8_MAX;
 pub const __INTN_MIN = @compileError("unable to translate macro: undefined identifier `INT`");
-// C:\zig\zig\0.13.0\files\lib\include/stdint.h:871:10
+// C:\zig\zig\0.14.0-dev.2599+ff4f2753e\files\lib\include/stdint.h:875:10
 pub const __INTN_MAX = @compileError("unable to translate macro: undefined identifier `INT`");
-// C:\zig\zig\0.13.0\files\lib\include/stdint.h:872:10
+// C:\zig\zig\0.14.0-dev.2599+ff4f2753e\files\lib\include/stdint.h:876:10
 pub const __UINTN_MAX = @compileError("unable to translate macro: undefined identifier `UINT`");
-// C:\zig\zig\0.13.0\files\lib\include/stdint.h:873:9
+// C:\zig\zig\0.14.0-dev.2599+ff4f2753e\files\lib\include/stdint.h:877:9
 pub const __INTN_C = @compileError("unable to translate macro: undefined identifier `INT`");
-// C:\zig\zig\0.13.0\files\lib\include/stdint.h:874:10
+// C:\zig\zig\0.14.0-dev.2599+ff4f2753e\files\lib\include/stdint.h:878:10
 pub const __UINTN_C = @compileError("unable to translate macro: undefined identifier `UINT`");
-// C:\zig\zig\0.13.0\files\lib\include/stdint.h:875:9
+// C:\zig\zig\0.14.0-dev.2599+ff4f2753e\files\lib\include/stdint.h:879:9
 pub const INTPTR_MIN = -__INTPTR_MAX__ - @as(c_int, 1);
 pub const INTPTR_MAX = __INTPTR_MAX__;
 pub const UINTPTR_MAX = __UINTPTR_MAX__;
@@ -882,13 +893,13 @@ pub inline fn UINTMAX_C(v: anytype) @TypeOf(__int_c(v, __UINTMAX_C_SUFFIX__)) {
     return __int_c(v, __UINTMAX_C_SUFFIX__);
 }
 pub const APIENTRY = @compileError("unable to translate C expr: unexpected token '__stdcall'");
-// include\GLFW\glfw3.h:123:11
+// glfw3.h:123:11
 pub const GLFW_APIENTRY_DEFINED = "";
 pub const WINGDIAPI = @compileError("unable to translate macro: undefined identifier `dllimport`");
-// include\GLFW\glfw3.h:133:10
+// glfw3.h:133:10
 pub const GLFW_WINGDIAPI_DEFINED = "";
 pub const CALLBACK = @compileError("unable to translate C expr: unexpected token '__stdcall'");
-// include\GLFW\glfw3.h:140:10
+// glfw3.h:140:10
 pub const GLFW_CALLBACK_DEFINED = "";
 pub const GLFWAPI = "";
 pub const GLFW_VERSION_MAJOR = @as(c_int, 3);
